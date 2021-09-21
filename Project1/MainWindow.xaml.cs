e5d594a0c403c07eb0238fb22af4c4f3d374daa1
Chester Brock Jr <W7259378@student.miracosta.edu>
2021-09-21 13:50:27 -0700

    Add project files.

---
+++ Project1/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿/// Chapter No. 4		Exercise No. 1
+/// File Name: MainWindow.xaml.cs
+/// @author: Chester Brock 
+/// Date:  Sep 20, 2021
+///
+/// Problem Statement: Write a GUI program that reads in the amount of a monthly mortgage payment and the outstanding balance  
+/// 
+/// 
+/// Overall Plan:
+/// 1) Create a constant for the yearly interest tax rate
+/// 2) Prompt user to enter outstanding balance and monthly payments 
+/// 3) get the monthly interest rate by dividing by 12 (6.39 * outstanding balance) / 12)
+/// 4) Calculate the amount that goes into interest 
+/// 5) Find the principal amount 
+/// 6) find new balance 
+/// 7) Display all the results 
+///
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Project1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Project2/Program.cs
@@ -0,0 +1,32 @@
+﻿/// Chapter No. 4		Exercise No. 2
+/// File Name: Program.cs
+/// @author: Chester Brock 
+/// Date:  Sep 20, 2021
+///
+/// Problem Statement: Write a program (Console or GUI) that determines the change to be dispensed from a vending machine.
+/// 
+/// 
+/// Overall Plan:
+/// 1) Prompt the user for the amount of the item ( 5 cent increments up to a dollar) 
+/// 2) Calculate everything with pennys 
+/// 3) convert the amount of pennies each coin holds and save them as integers
+/// 4) Starting with quarters use the value of the change and divide by the amount of pennies it holds (25)
+/// 5) Next, enter the dime field by using the % sign with the change and the amount of quarters. 
+/// 6) epeat this step again but with dimes and nickels as the factors .
+/// 7) display the results to the user. 
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
